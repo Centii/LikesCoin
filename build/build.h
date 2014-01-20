62cb288d120dad8553c0a2390b65be2356ee102a
stewjohn <stewjohn@centii.us>
2014-01-20 15:04:07 -0800

    build update

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-01-10 15:42:17 -0500"
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("LIKES");
+    case mBTC: return QString("mLIKES");
+    case uBTC: return QString::fromUtf8("μLIKES");
     default: return QString("???");
     }
 }
